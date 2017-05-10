c3887a6428cfcb7c5eb848195a387f155f475a86
RanikaR <rmr29@njit.edu>
2017-05-10 16:09:15 -0400

    Updated database info

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql1.njit.edu;dbname=rmr29';
+    $username = 'rmr29';
+    $password = 'UEF6rZEE4';
 
     try {
         $db = new PDO($dsn, $username, $password);
